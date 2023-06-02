57b88a359405819b80d79ff5ecbde4505968e333
Hyunwoo Cho <truetype24@gmail.com>
2023-06-02 21:35:14 +0900

    chore: writing validator pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { EnvModule } from './common/modules/config.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: process.env.NODE_ENV == 'dev' ? '.env.dev' : '.env',
-    }),
-  ],
+  imports: [EnvModule],
   controllers: [AppController],
   providers: [AppService],
 })
