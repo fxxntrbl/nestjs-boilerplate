58c32dc7005055b8f9f9676706300a5dbdea302a
Hyunwoo Cho <truetype24@gmail.com>
2023-06-02 20:41:06 +0900

    feat: using cross-env for various production status

---
+++ src/common/swagger.ts
@@ -0,0 +1,1 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { Logger } from '@nestjs/common';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import helmet from 'helmet';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(helmet({ contentSecurityPolicy: false }));
+
+  if (process.env.NODE_ENV == 'dev') {
+    Logger.debug('Running as DEV mode');
+  }
+
+  const port = parseInt(process.env.PORT) || 8080;
+  await app.listen(port);
 }
 bootstrap();
